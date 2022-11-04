deaa65e9a25844b3229312914acb6d475911e2db
aeferreira <ferreiae@gmail.com>
2022-11-04 13:55:10 +0000

    app.py finished.

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 """Preparation of files for KEGG Mapper."""
 
-from io import StringIO, BytesIO
+from io import StringIO
 
 import panel as pn
-pn.extension('tabulator')
 pn.extension('notifications')
 pn.extension(notifications=True)
 
+def get_cid(data):
+    cid = []
+    for line in data.splitlines():
+        ID = line.split("\t")[6]
+        if ID.startswith("C"):
+            cid.append(ID)
+    return cid
 
+def compute(f1_content, f2_content):
+
+    cid1 = get_cid(f1_content)
+    cid2 = get_cid(f2_content)
+
+    # join the two lists, removing duplicates
+    allcompounds = list( dict.fromkeys(cid1 + cid2))
+
+    # transform into sets (to compute set intersection)
+
+    set1 = set(cid1)
+    set2 = set(cid2)
+    both = set1.intersection(set2)
+    only1 = set1.difference(both)
+    only2 = set2.difference(both)
+    
+    results = {'n1': len(only1), 'n2': len(only2), 'n_common': len(both)}
+
+    text = ["KEGG_ID\tcolor"]
+
+    for compound in allcompounds:
+        if compound in both:
+            color = 'yellow'
+        elif compound in set1:
+            color = 'blue'
+        else:
+            color = 'red'
+
+        text.append( f'{compound}\t{color}' )
+    text = '\n'.join(text)
+    results['map'] = text
+    return results
 
 # widgetry
 
@@ -30,23 +68,60 @@ Tool developed by *António Ferreira*, *Alexandre Coelho* and *Henrique Silva*
 
 -------''')
 
+results = {}
+
 file_input1 = pn.widgets.FileInput()
 file_input2 = pn.widgets.FileInput()
 
-# the "Clear" button
-clear_button = pn.widgets.Button(name='Clear', width=200)
-def b_clear(event):
-    file_input1.value = None
-    file_input2.value = None
+def b_compute(event):
+    out_msg.object = ''
+    if file_input1.value is None and file_input2.value is None:
+        out_msg.object = 'No files were selected'
+        return
+    if file_input1.value is None:
+        out_msg.object = 'No file 1 was selected'
+        return
+    if file_input2.value is None:
+        out_msg.object = 'No file 2 was selected'
+        return
+    try:
+        f1_content = file_input1.value.decode('utf-8')
+        f2_content = file_input2.value.decode('utf-8')
+    except UnicodeDecodeError:
+        f1_content = file_input1.value.decode('ISO-8859-1')
+        f2_content = file_input2.value.decode('ISO-8859-1')
+    name1 = file_input1.filename
+    name2 = file_input2.filename
 
-clear_button.on_click(b_clear)
-action_buttons = pn.Row(clear_button)
+    results = compute(f1_content, f2_content)
 
-file_inputs = pn.Column(pn.widgets.StaticText(name='File 1', value=' '),
+    numbers = [f'<p>There are <span style="color:goldenrod;">{results["n_common"]}</span> common KeGG IDs in both files</p>',
+            f'<p>There are <span style="color:darkblue;">{results["n1"]}</span> exclusive KeGG IDs in file <span style="color:darkblue;">{name1}</span></p>',
+            f'<p>There are <span style="color:red;">{results["n2"]}</span> exclusive KeGG IDs in file <span style="color:red;">{name2}</span></p>']
+
+    out_msg.object = '\n'.join(numbers)
+
+    sio = StringIO()
+    sio.write(results['map'])
+    sio.seek(0)
+    d_button = pn.widgets.FileDownload(sio, embed=True,
+                                       filename='map.txt',
+                                       label='Download map.txt',
+                                       button_type='success',
+                                       width=200)
+    app_column[-1] = d_button
+
+compute_button = pn.widgets.Button(name='Compute', width=200, button_type='primary')
+compute_button.on_click(b_compute)
+
+file_inputs = pn.Column(pn.widgets.StaticText(name='File 1', value = ' '),
                         file_input1,
-                        pn.widgets.StaticText(name='File 2', value=' '),
+                        pn.widgets.StaticText(name='File 2', value = ' '),
                         file_input2)
 
-app_column = pn.Column(header, file_inputs, action_buttons, width=500)
+
+out_msg = pn.pane.Markdown('', width=680)
+bogus_button = pn.widgets.Button(visible=False)
+app_column = pn.Column(header, file_inputs, compute_button, out_msg, bogus_button, width=700)
 
 app_column.servable()
